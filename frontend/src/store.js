6f4504a34b174caf508cb0d016168eaa2d054b6a
Rauno Uustalu <raunoize@gmail.com>
2020-12-09 20:49:12 +0200

    Place order vol1

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,8 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
-
+import { orderCreateReducer } from './reducers/orderReducers'
+ 
 
 
 const reducer = combineReducers({
@@ -15,14 +16,17 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage},
+  cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage},
   userLogin: { userInfo: userInfoFromStorage }
 }
 
