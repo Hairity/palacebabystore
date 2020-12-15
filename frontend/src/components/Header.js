d31cbe8d14823cd3cc1908aa281bfaf68748bbdf
Rauno Uustalu <raunoize@gmail.com>
2020-12-15 12:01:13 +0200

    FULLY FUNCTIONAL MERN STACK STORE

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,32 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 
 const Header = () => {
   const dispatch = useDispatch()
 
-  const userLogin = useSelector(state => state.userLogin)
+  const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
   const logoutHandler = () => {
     dispatch(logout())
   }
+
   return (
     <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect >
+      <Navbar style={{color: 'red'}} bg='info' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-          <Navbar.Brand>PalaceBaby</Navbar.Brand>
+            <Navbar.Brand>PalaceBaby</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Route render={({ history }) => <SearchBox history={history} />} />
+            <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
@@ -44,27 +48,24 @@ const Header = () => {
                   </Nav.Link>
                 </LinkContainer>
               )}
-                {userInfo && userInfo.isAdmin && (
-                  <NavDropdown title='Admin' id='adminmenu'>
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
                   <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-
                   <LinkContainer to='/admin/productlist'>
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
-
                   <LinkContainer to='/admin/orderlist'>
                     <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
-                 
                 </NavDropdown>
-                )}
-              </Nav>
-            </Navbar.Collapse>
+              )}
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-          </Navbar>
-              </header>
+      </Navbar>
+    </header>
   )
 }
 
